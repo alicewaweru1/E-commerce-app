52dee012f6ff12e42ef7adf6c0ef6d233685973e
alicewaweru1 <alicewaweru003@5676>
2026-05-26 13:38:37 +0300

    chore: configure dynamic base path for local dev and github-pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/E-commerce-app/',
-  plugins: [react(), tailwindcss()],
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/E-commerce-app/' : '/',
+    plugins: [react(), tailwindcss()],
+  }
 })
